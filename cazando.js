a76ea355c86e35cc582c5ccf283f931857aeb92a
Norfolk <alexander.garofalo.ag@gmail.com>
2026-04-09 00:58:11 -0500

    Taller 1 - Paso 12 al 19 - Creacion funciones graficarGato, graficarComida, inicarJuego, definir variables y constantes

--- cazando.js
+++ cazando.js
@@ -8,23 +8,34 @@ let gatoY = 0;
 let comidaX = 0;
 let comidaY = 0;
 
+// Constantes de tamaño
+const ANCHO_GATO = 60;
+const ALTO_GATO = 60;
+const ANCHO_COMIDA = 30;
+const ALTO_COMIDA = 30;
+
+
 function graficarRectangulo(x, y, ancho, alto, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, ancho, alto);
 }
 
 function graficarGato() {
-  graficarRectangulo(gatoX, gatoY, 50, 50, "red");
+  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "red");
 }
 
 function graficarComida() {
-  graficarRectangulo(comidaX, comidaY, 30, 30, "gray");
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "gray");
 }
 
 function iniciarJuego() {
   // Gato centrado
   gatoX = (canvas.width  - 50)  / 2;
   gatoY = (canvas.height - 50)   / 2;
+
+  comidaX = canvas.width  - ANCHO_COMIDA - 10;
+  comidaY = canvas.height - ALTO_COMIDA  - 10;
+
     graficarGato();
     graficarComida();
 }
