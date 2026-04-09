f960964108e625842a4e790ecd8ca54582f69549
Norfolk <alexander.garofalo.ag@gmail.com>
2026-04-09 00:37:03 -0500

    Taller 1 - Paso 8 al 11 - Creado funcion graficargato y evento onload

--- cazando.js
+++ cazando.js
@@ -1,3 +1,25 @@
 // Canvas y contexto
-var canvas = document.getElementById("areaJuego");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+
+// Variables de posición
+let gatoX = 0;
+let gatoY = 0;
+
+function graficarRectangulo(x, y, ancho, alto, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, ancho, alto);
+}
+
+function graficarGato() {
+  graficarRectangulo(gatoX, gatoY, 50, 50, "red");
+}
+
+function iniciarJuego() {
+  // Gato centrado
+  gatoX = (canvas.width  - 50)  / 2;
+  gatoY = (canvas.height - 50)   / 2;
+    graficarGato();
+}
+    
+iniciarJuego();
