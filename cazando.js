b3b5866911bdab9237296c705a48817e90470090
Norfolk <alexander.garofalo.ag@gmail.com>
2026-04-09 23:39:43 -0500

    Taller 1.P2 - Parte 3.1 - Creacion de la funcion limparCanva js

--- cazando.js
+++ cazando.js
@@ -1,43 +1,51 @@
-// Canvas y contexto
-let canvas = document.getElementById("areaJuego");
+let canvas = document.getElementById("areajuego");
 let ctx = canvas.getContext("2d");
+const VELOCIDAD = 15;
 
-// Variables de posición
+ 
+// Gato
 let gatoX = 0;
 let gatoY = 0;
-let comidaX = 0;
-let comidaY = 0;
-
-// Constantes de tamaño
-const ANCHO_GATO = 60;
-const ALTO_GATO = 60;
+const ANCHO_GATO = 50;
+const ALTO_GATO = 50;
+ 
+// Comida
+let comidaX = 50;
+let comidaY = 50;
 const ANCHO_COMIDA = 30;
 const ALTO_COMIDA = 30;
-
-
+ 
 function graficarRectangulo(x, y, ancho, alto, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, ancho, alto);
-}
-
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+};
+ 
 function graficarGato() {
-  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "red");
-}
-
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#000000");
+};
+ 
 function graficarComida() {
-  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "gray");
-}
-
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#ff0000");
+};
+ 
 function iniciarJuego() {
-  // Gato centrado
-  gatoX = (canvas.width  - 50)  / 2;
-  gatoY = (canvas.height - 50)   / 2;
-  comidaX = canvas.width  - ANCHO_COMIDA - 10;
-  comidaY = canvas.height - ALTO_COMIDA  - 10;
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
 
-    graficarRectangulo(0, 0, canvas.width, canvas.height, "white");
+      //COMIDA ESQUINA INFERIOR DERECHA
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+ 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     graficarGato();
     graficarComida();
 }
-    
-iniciarJuego();
+ 
+function limpiarCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+document.getElementById("btnArriba").onclick = () => moverArriba();
+document.getElementById("btnAbajo").onclick = () => moverAbajo();
+document.getElementById("btnIzquierda").onclick = () => moverIzquierda();
+document.getElementById("btnDerecha").onclick = () => moverDerecha();
